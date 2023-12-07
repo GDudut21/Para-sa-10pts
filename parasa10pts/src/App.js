92510e352497d34fca0f4bbf4b542314b6c73fec
GDudut21 <girondudut92@gmail.com>
2023-12-07 14:19:15 +0800

    update

--- parasa10pts/src/App.js
+++ parasa10pts/src/App.js
@@ -5,6 +5,12 @@ function App() {
   const name = <h1>"Giron"</h1>
   const age = <h2>21</h2>
   const email = <h2>giron@.edu.ph</h2>
+  const user =(<div>
+    {name}
+    {age}
+    {email}
+    </div>);
+  }
   return (
     <div className="App">
      {name}
@@ -12,6 +18,5 @@ function App() {
      {email}
     </div>
   );
-}
 
 export default App;
