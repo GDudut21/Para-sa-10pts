32de1132d5a9b3a47671ea47c7a1fee3a8661e41
GDudut21 <girondudut92@gmail.com>
2023-12-07 14:39:40 +0800

    Update app.jss
    Added the age voter bases on today lecture

--- parasa10pts/src/App.js
+++ parasa10pts/src/App.js
@@ -1,22 +1,19 @@
 
 import './App.css';
+import { useState } from "react";
 
-function App() {
-  const name = <h1>"Giron"</h1>
-  const age = <h2>21</h2>
-  const email = <h2>giron@.edu.ph</h2>
-  const user =(<div>
-    {name}
-    {age}
-    {email}
-    </div>);
-  }
-  return (
-    <div className="App">
-     {name}
-     {age}
-     {email}
-    </div>
-  );
+function app () {
+  const age = 18;
+}
 
-export default App;
+if (age >= 18) {
+  return <h1> POIDE NA MAG LOVE LIFE</h1>
+}  else {
+  return <div className="App">
+  <h1>BAWAL PA MAG LOVE LIFE !</h1>
+  </div>
+}
+
+return <div className="App">
+  
+</div>
