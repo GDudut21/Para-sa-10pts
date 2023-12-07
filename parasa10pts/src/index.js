624cffd16c789521047665a6af2742be69d22c6b
GDudut21 <girondudut92@gmail.com>
2023-12-07 15:46:21 +0800

    Update

--- parasa10pts/src/index.js
+++ parasa10pts/src/index.js
@@ -1,7 +1,21 @@
-export const User = (props) => {
+import React from 'react';
+import { User } from './User.js';
+import './App.css'; // Import the CSS file
+
+function App() {
+  const planets = [
+    // ... (unchanged)
+  ];
+
   return (
-    <div>
-      {props.name} {props.age}
-   </div>
- )
+    <div className="App">
+      {planets.map((planet, key) => {
+        return (
+          <User key={key} name={planet.planetName} isRockPlanet={planet.isRockPlanet} />
+        );
+      })}
+    </div>
+  );
 }
+
+export default App;
